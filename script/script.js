f871c7dccfc0a0144f12c64f0af0d54a51ae14cc
Marufjon94f <faith94nazarov@yandex.ru>
2021-07-15 11:53:01 +0500

    add

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+function currencyConverter(valNum){
+   
+    
+    document.getElementById("p").value = (valNum/3.6).toFixed(2);
+    document.getElementById("v").value = (valNum/20.1 ).toFixed(2);
+    document.getElementById("m").value = (valNum/70).toFixed(2);
+    document.getElementById("s").value = (valNum/800).toFixed(2);
+    
+    }
+    
